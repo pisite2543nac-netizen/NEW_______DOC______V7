30b3a6b57a90eb5cc1f051c6dce1443f29e26476
Pisit2000 <Pisite2543nac@gmail.com>
2026-09-10 09:01:55 +0700

    UI batch 01 registration dashboard worksheet style

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE="doc-full-nr-complete-production-20260910-v2";
+const CACHE="doc-full-nr-ui-batch-01-20260910";
 const SHELL=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
