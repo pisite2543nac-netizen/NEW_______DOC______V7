6bc2c82e38f3f0df69a2bf882177d1a5b21820e8
pisite2543nac-netizen <pisite.2543nac@gmail.com>
2026-09-15 08:35:40 +0700

    Hotfix V16.8 course-flow JavaScript syntax for V16.9 deployment

--- site/v16-8-course-flow.js
+++ site/v16-8-course-flow.js
@@ -35,7 +35,7 @@ function client(){
   const s=readSession();
   return createClient(SUPABASE_URL,SUPABASE_KEY,{
     auth:{persistSession:false,autoRefreshToken:false,detectSessionInUrl:false},
-    global:{headers:s?.access_token?{Authorization:`Bearer ${s.access_token}`}:{}}}
+    global:{headers:s?.access_token?{Authorization:`Bearer ${s.access_token}`}:{}}
   });
 }
 function flash(message,bad=false){
